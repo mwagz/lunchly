b0b76dbd73316e1487baf33e1dbac8b6d629638f
Matt Wagner <matt.a.wagner4@gmail.com>
2018-04-04 11:45:55 -0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  places: any[];
+  noThai: boolean;
+  selectedPlace: string;
+
+  constructor() {
+    this.places = [
+      'Qdoba',
+      'Firehouse',
+      'Yats',
+      'Five Guys',
+      'Subway',
+      'Thai'
+    ];
+
+    this.selectedPlace = 'Click the button';
+  }
+
+  findPlace(): void {
+    let place = this.randomPlace();
+    if (this.noThai && place === 'Thai') {
+      this.findPlace();
+      return;
+    }
+    this.selectedPlace = place;
+  }
+
+  randomPlace(): string {
+    return this.places[Math.floor(Math.random() * this.places.length)];
+  }
 }
