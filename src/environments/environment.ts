608f5cac7d865374863c2880072850ff3f0d357f
Matt Wagner <matt.a.wagner4@gmail.com>
2018-04-04 11:51:58 -0400

    Remove Firebase stuff

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,5 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: 'AIzaSyCPjG5kmMl4K1cHiOB49_cgtLk6MhxgUqQ',
-    authDomain: 'lunchly-34559.firebaseapp.com',
-    databaseURL: 'https://lunchly-34559.firebaseio.com',
-    projectId: 'lunchly-34559',
-    storageBucket: 'lunchly-34559.appspot.com',
-    messagingSenderId: '448743394845'
-  }
+  production: false
 };
